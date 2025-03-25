2440e8856c3f5502f3c2cbd6c4420fbfa8f220d5
jadeeg <jadeegoncalves@gmail.com>
2025-03-25 11:22:28 -0300

    css animation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
+import "animate.css";
 
-  function time (){
-     let citytime = document.getElementById("select").value; 
-     if(citytime) { 
-      let info = document.getElementById("info");
-    
-    let date = `${moment().tz(citytime).format("MMMM Do YYYY")}`; 
-    let timenow = `${moment().tz(citytime).format("h:mm:ss [<small>] a [</small>]")}`;
+function time() {
+  let citytime = document.getElementById("select").value;
+  if (citytime) {
+    let info = document.getElementById("info");
+
+    let date = `${moment().tz(citytime).format("MMMM Do YYYY")}`;
+    let timenow = `${moment()
+      .tz(citytime)
+      .format("h:mm:ss [<small>] a [</small>]")}`;
     let selectCitytime = citytime.split("/")[1];
-    
+
     info.innerHTML = `${date} ${timenow}  <div id= "city" > ${selectCitytime} </div>`;
-    
-     } 
-     else if  (citytime === "userTime") 
-     {
-              let info = document.getElementById("info");
-              info.innerHTML = new Date().toString(); 
-     }
+  } else if (citytime === "userTime") {
+    let info = document.getElementById("info");
+    info.innerHTML = new Date().toString();
   }
+}
 
 document.getElementById("select").addEventListener("change", time);
-
-
