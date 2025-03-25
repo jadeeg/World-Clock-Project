b9ca7e4b67444eb28734cd0123088179a0a7d195
jadeeg <jadeegoncalves@gmail.com>
2025-03-24 21:23:10 -0300

    js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+  function time (){
+     let citytime = document.getElementById("select").value; 
+     if(citytime) { 
+      let info = document.getElementById("info");
+    
+    let date = `${moment().tz(citytime).format("MMMM Do YYYY")}`; 
+    let timenow = `${moment().tz(citytime).format("h:mm:ss [<small>] a [</small>]")}`;
+    let selectCitytime = citytime.split("/")[1];
+    
+    info.innerHTML = `${date} ${timenow}  <div id= "city" > ${selectCitytime} </div>`;
+    
+     } 
+     else if  (citytime === "userTime") 
+     {
+              let info = document.getElementById("info");
+              info.innerHTML = new Date().toString(); 
+     }
+  }
+
+document.getElementById("select").addEventListener("change", time);
+
+
